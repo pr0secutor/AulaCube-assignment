beb29f01c99ea0598100a486c4a2f5fc8604eb1d
pr0secutor <ifahim3107@gmail.com>
2024-02-25 11:05:01 +0530

    initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='content-container'>
+      <h1>Gameplay Update 7.34e and What’s Up Next</h1>
+      <img src='dota.png' alt='content-image'/>
+      <p>After nearly a year of coordination and production, the multi-week, arena-sized monster that is The International 2023 has come to an exciting close. We've locked up Climate Pledge Arena, we've all seen enough of Spirit Breaker, and we've just shipped Gameplay Update 7.34e. Time for the Dota team to settle down for a long winter's nap, and...<br/><br/>
+But hark! What's that sound in the festive distance? No, that jingling of sleigh bells you hear isn't early onset tinnitus. It's the magical year-end event we call Frostivus — looming large on the December calendar, and arriving with a sack stuffed to bursting with new features, new cosmetics, and some surprises.</p>
+    </div>
+  )
+}
+
+export default Content
